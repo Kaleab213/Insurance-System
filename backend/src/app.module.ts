7f4f99f020efaa2dc542d4982fe513f793209413
fasika <fasikafikadu38@gmail.com>
2023-05-22 00:16:05 +0300

    Removed node_modules directory from Git tracking

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './Authentication/Auth.module';
 import { PrismaModule } from './Prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { RequestModule } from './request/request.module';
+import { PaymentModule } from './payment/payment.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [AuthModule,PrismaModule,ConfigModule.forRoot({isGlobal:true})],
+  imports: [AuthModule,PrismaModule,ConfigModule.forRoot({isGlobal:true}), RequestModule, PaymentModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
