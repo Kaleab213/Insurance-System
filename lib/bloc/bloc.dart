71b8733524187b9785b3d37e0494b190ae960932
Enyew <enyewanb0000@gmail.com>
2023-05-23 10:17:12 +0300

    added

---
+++ lib/bloc/bloc.dart
@@ -0,0 +1,3 @@
+export 'insurance_bloc.dart';
+export 'insurance_event.dart';
+export 'insurance_state.dart';
---
+++ lib/bloc/insurance_event.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+import 'package:insurancesystem/insurance/insurance.dart';
+
+abstract class InsuranceEvent extends Equatable {
+  const InsuranceEvent();
+}
+
+class InsuranceLoad extends InsuranceEvent {
+  const InsuranceLoad();
+
+  @override
+  List<Object> get props => [];
+}
+
+class InsuranceCreate extends InsuranceEvent {
+  final Insurance insurance;
+
+  const InsuranceCreate(this.insurance);
+
+  @override
+  List<Object> get props => [insurance];
+
+  @override
+  String toString() => 'Insurance Created {insurance: $insurance}';
+}
+
+class InsuranceUpdate extends InsuranceEvent {
+  final Insurance insurance;
+
+  const InsuranceUpdate(this.insurance);
+
+  @override
+  List<Object> get props => [insurance];
+
+  @override
+  String toString() => 'Insurance Updated {insurance: $insurance}';
+}
+
+class InsuranceDelete extends InsuranceEvent {
+  final Insurance insurance;
+
+  const InsuranceDelete(this.insurance);
+
+  @override
+  List<Object> get props => [insurance];
+
+  @override
+  String toString() => 'Insurance Deleted {insurance: $insurance}';
+}
